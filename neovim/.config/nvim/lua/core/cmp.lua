fc4195da3844b661e430fad6493fa43865c688a7
Richard Roncancio <batusai513@msn.com>
2022-03-04 11:43:52 +0100

    chore: better cmp motions

--- neovim/.config/nvim/lua/core/cmp.lua
+++ neovim/.config/nvim/lua/core/cmp.lua
@@ -1,4 +1,9 @@
 local icons = require "core.theme.icons"
+local lspUtils = require "core.utils.lsp"
+
+local jumpable = lspUtils.jumpable
+local is_emmet_active = lspUtils.is_emmet_active
+local check_backspace = lspUtils.check_backspace
 
 local cmp = prequire "cmp"
 if not cmp then
@@ -12,21 +17,33 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_backspace = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-end
-
 --   פּ ﯟ   some other good icons
 local kind_icons = icons.kind_icons
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
+local confirm_opts = {
+  behavior = cmp.ConfirmBehavior.Replace,
+  select = false,
+}
+
 cmp.setup {
+  confirm_opts = confirm_opts,
+  documentation = {
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  },
+  completion = {
+    keyword_length = 1,
+  },
+  experimental = {
+    ghost_text = true,
+    native_menu = false,
+  },
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
+  duplicates_default = 0,
   mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
     ["<C-j>"] = cmp.mapping.select_next_item(),
@@ -40,16 +57,33 @@ cmp.setup {
     },
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<CR>"] = cmp.mapping(function(fallback)
+      if cmp.visible() and cmp.confirm(confirm_opts) then
+        if jumpable() then
+          luasnip.jump(1)
+        end
+        return
+      end
+
+      if jumpable() then
+        if not luasnip.jump(1) then
+          fallback()
+        end
+      else
+        fallback()
+      end
+    end),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expandable() then
         luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+      elseif jumpable() then
+        luasnip.jump(1)
       elseif check_backspace() then
         fallback()
+      elseif is_emmet_active() then
+        return vim.fn["cmp#complete"]()
       else
         fallback()
       end
@@ -60,7 +94,7 @@ cmp.setup {
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      elseif jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
@@ -91,16 +125,5 @@ cmp.setup {
     { name = "buffer" },
     { name = "path" },
     { name = "nvim_lsp_signature_help" },
-  },
-  confirm_opts = {
-    behavior = cmp.ConfirmBehavior.Replace,
-    select = false,
-  },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  },
-  experimental = {
-    ghost_text = false,
-    native_menu = false,
   },
 }
