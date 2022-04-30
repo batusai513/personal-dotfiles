dff830e54aea9e3fad7670741095f0b5fa6b4027
Richard Roncancio <batusai513@msn.com>
2022-04-30 18:09:03 +0200

    chore: fix lofig error for lsp server initialization

--- neovim/.config/nvim/lua/core/lsp/lsp-installer.lua
+++ neovim/.config/nvim/lua/core/lsp/lsp-installer.lua
@@ -13,14 +13,14 @@ local function on_server_ready(server)
   local has_custom_config, server_custom_config = pcall(require, "core.lsp.server_settings." .. server.name)
   if has_custom_config then
     if type(server_custom_config) == "function" then
-      server_custom_config(server, opts)
+      return server_custom_config(server, opts)
     else
       opts = vim.tbl_deep_extend("force", opts, server_custom_config)
-      -- This setup() function is exactly the same as lspconfig's setup function.
-      -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-      server:setup(opts)
     end
   end
+  -- This setup() function is exactly the same as lspconfig's setup function.
+  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  server:setup(opts)
 end
 
 lsp_installer.on_server_ready(on_server_ready)
