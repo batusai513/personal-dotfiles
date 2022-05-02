0613b6c9020f6ff9c2fb3baea14982063c2c2b20
Richard Roncancio <batusai513@msn.com>
2022-05-02 13:56:17 +0200

    feat: show lualine relative file path

--- neovim/.config/nvim/lua/core/lualine/init.lua
+++ neovim/.config/nvim/lua/core/lualine/init.lua
@@ -14,4 +14,13 @@ lualine.setup {
     "nvim-tree",
     "toggleterm",
   },
+  sections = {
+    lualine_c = {
+      {
+        "filename",
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+      },
+    },
+  },
 }
