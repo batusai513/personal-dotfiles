0d60d99945796967d2c4cb9e99cac575a544e7b4
Richard Roncancio <batusai513@msn.com>
2022-05-21 08:33:20 +0200

    chore: update autocommands

--- neovim/.config/nvim/lua/core/autocommands.lua
+++ neovim/.config/nvim/lua/core/autocommands.lua
@@ -1,28 +1,6 @@
 vim.cmd [[
-  augroup _general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd FileType qf set nobuflisted
-  augroup end
-
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd FileType gitcommit setlocal spell
-  augroup end
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
-  augroup end
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd = 
-  augroup end
   augroup _alpha
     autocmd!
-
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
 ]]
@@ -35,6 +13,30 @@ vim.cmd "au ColorScheme * hi TelescopeBorder ctermbg=none guibg=none"
 vim.cmd "au ColorScheme * hi NvimTreeNormal ctermbg=none guibg=none"
 vim.cmd "au ColorScheme * hi EndOfBuffer ctermbg=none guibg=none"
 vim.cmd "let &fcs='eob: '"
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+  callback = function()
+    vim.cmd [[
+      nnoremap <silent> <buffer> q :close<CR> 
+      set nobuflisted 
+    ]]
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd "tabdo wincmd ="
+  end,
+})
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
