066f9b62abce299d83da198ac4ff262599bddfb1
Richard Roncancio <batusai513@msn.com>
2022-05-21 08:48:56 +0200

    chore: update whichkey

--- neovim/.config/nvim/lua/core/which_key.lua
+++ neovim/.config/nvim/lua/core/which_key.lua
@@ -5,14 +5,11 @@ local config = {
     marks = true, -- shows a list of your marks on ' and `
     registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-
     -- No actual key bindings are created
     presets = {
-
       operators = false, -- adds help for operators like d, y, ...
       motions = false, -- adds help for motions
       text_objects = false, -- help for text objects triggered after entering an operator
-
       windows = true, -- default bindings on <c-w>
       nav = true, -- misc bindings to work with windows
       z = true, -- bindings for folds, spelling and others prefixed with z
@@ -25,6 +22,10 @@ local config = {
     separator = "➜", -- symbol used between a key and it's label
     group = "+", -- symbol prepended to a group
   },
+  popup_mappings = {
+    scroll_down = "<c-d>", -- binding to scroll down inside the popup
+    scroll_up = "<c-u>", -- binding to scroll up inside the popup
+  },
   window = {
     border = "single", -- none, single, double, shadow
     position = "bottom", -- bottom, top
@@ -34,8 +35,10 @@ local config = {
   layout = {
     height = { min = 4, max = 25 }, -- min and max height of the columns
     width = { min = 20, max = 50 }, -- min and max width of the columns
-    spacing = 3, -- spacing between columns
+    spacing = 6, -- spacing between columns
+    align = "center",
   },
+  ignore_missing = true,
   hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
   show_help = true, -- show help message on the command line when the popup is visible
   opts = {
@@ -179,6 +182,15 @@ local config = {
         "Colorscheme with Preview",
       },
     },
+    t = {
+      name = "+Trouble",
+      r = { "<cmd>Trouble lsp_references<cr>", "References" },
+      f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+      d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+      q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+      l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+      w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
+    },
     ["\\"] = {
       name = "Terminal",
       ["1"] = { ":1ToggleTerm<cr>", "1" },
@@ -188,15 +200,6 @@ local config = {
       h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
       v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
       m = { "<cmd>lua _Htop_toggle()<cr>", "HTOP" },
-    },
-    t = {
-      name = "+Trouble",
-      r = { "<cmd>Trouble lsp_references<cr>", "References" },
-      f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-      d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-      q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-      l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-      w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
     },
   },
 }
