d9e1a2660574bd843199627d2a9846a1681c7d19
Richard Roncancio <batusai513@msn.com>
2022-06-07 16:33:44 +0200

    chore: disble tsserver formating capabilities

--- neovim/.config/nvim/lua/core/lsp/default_attach.lua
+++ neovim/.config/nvim/lua/core/lsp/default_attach.lua
@@ -66,6 +66,11 @@ M.on_attach = function(client, bufnr)
     client.resolved_capabilities.document_range_formatting = false
   end
 
+  if client.name == "tsserver" then
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  end
+
   lsp_code_lens_refresh(client)
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
