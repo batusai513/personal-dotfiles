f41b0342a3a755e907d675166046557cadd7a633
Richard Roncancio <batusai513@msn.com>
2022-08-20 10:20:05 +0200

    chore: use master rust tools

--- neovim/.config/nvim/lua/core/plugins.lua
+++ neovim/.config/nvim/lua/core/plugins.lua
@@ -97,7 +97,6 @@ return packer.startup(function(use)
   --rust
   use {
     "simrat39/rust-tools.nvim",
-    branch = "modularize_and_inlay_rewrite",
     requires = {
       { "mfussenegger/nvim-dap" },
     },
