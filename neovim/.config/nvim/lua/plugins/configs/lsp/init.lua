a737d73f1d6ce7fa0e1580d19f11124f0ca90009
Richard Roncancio <batusai513@msn.com>
2022-10-05 14:48:48 +0200

    chore: remove capabilities already added by cmp_nvim_lsp update_capabilities

--- neovim/.config/nvim/lua/plugins/configs/lsp/init.lua
+++ neovim/.config/nvim/lua/plugins/configs/lsp/init.lua
@@ -63,18 +63,11 @@ function M.on_attach(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.foldingRange = {
   dynamicRegistration = false,
   lineFoldingOnly = true,
 }
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    "documentation",
-    "detail",
-    "additionalTextEdits",
-  },
-}
+
 M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 require("plugins.configs.lsp.handlers").setup()
