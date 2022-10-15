e5aad3b83b6234e5f336c6104fc3ebab38676116
Richard Roncancio <batusai513@msn.com>
2022-10-15 09:20:44 +0200

    chore: remove bufferline deprecation

--- neovim/.config/nvim/lua/core/bufferline.lua
+++ neovim/.config/nvim/lua/core/bufferline.lua
@@ -45,27 +45,6 @@ local options = {
   right_mouse_command = "vert sbuffer %d", -- can be a string | function, see "Mouse actions"
   left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
   middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-  -- NOTE: this plugin is designed with this icon in mind,
-  -- and so changing this is NOT recommended, this is intended
-  -- as an escape hatch for people who cannot bear it for whatever reason
-  indicator_icon = "▎",
-  buffer_close_icon = "",
-  -- buffer_close_icon = '',
-  modified_icon = "●",
-  close_icon = "",
-  -- close_icon = '',
-  left_trunc_marker = "",
-  right_trunc_marker = "",
-  --- name_formatter can be used to change the buffer's label in the bufferline.
-  --- Please note some names can/will break the
-  --- bufferline so use this at your discretion knowing that it has
-  --- some limitations that will *NOT* be fixed.
-  -- name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
-  --   -- remove extension from markdown files for example
-  --   if buf.name:match('%.md') then
-  --     return vim.fn.fnamemodify(buf.name, ':t:r')
-  --   end
-  -- end,
   max_name_length = 18,
   max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
   tab_size = 18,
@@ -114,7 +93,7 @@ local options = {
   -- [focused and unfocused]. eg: { '|', '|' }
   separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
   enforce_regular_tabs = true,
-  always_show_bufferline = true,
+  always_show_bufferline = false,
   sort_by = "id",
 }
 
