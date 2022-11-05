c6eecc616603afd5b0757b13584f8666df34047e
Richard Roncancio <batusai513@msn.com>
2022-11-05 14:52:41 +0100

    chore: update codelens
    chore: codelens

--- neovim/.config/nvim/lua/plugins/configs/lsp/codelens.lua
+++ neovim/.config/nvim/lua/plugins/configs/lsp/codelens.lua
@@ -1,8 +1,9 @@
+local group = vim.api.nvim_create_augroup("lsp_document_codelens", {})
+
 local function buf_autocmd_codelens(bufnr)
-  vim.api.nvim_create_augroup("lsp_document_codelens", {})
   vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "BufWritePost", "CursorHold" }, {
     buffer = bufnr,
-    group = "lsp_document_codelens",
+    group = group,
     callback = vim.lsp.codelens.refresh,
   })
 end
@@ -14,5 +15,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
       buf_autocmd_codelens(args.buf)
       vim.schedule(vim.lsp.codelens.refresh)
     end
+  end,
+})
+
+vim.api.nvim_create_autocmd("LspDetach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local bufnr = args.buf
+
+    vim.api.nvim_clear_autocmds {
+      buffer = bufnr,
+      group = group,
+    }
   end,
 })
