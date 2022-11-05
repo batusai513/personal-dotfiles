b4d2a25a9f8ed0c81dba316c899a4fc98a9ddf47
Richard Roncancio <batusai513@msn.com>
2022-11-05 15:43:21 +0100

    chore: keymaps

--- neovim/.config/nvim/lua/plugins/configs/lsp/keymaps.lua
+++ neovim/.config/nvim/lua/plugins/configs/lsp/keymaps.lua
@@ -29,6 +29,13 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local bufnr = args.buf
     local client = vim.lsp.get_client_by_id(args.data.client_id)
 
+    if client.server_capabilities.completionProvider then
+      vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+    end
+    if client.server_capabilities.definitionProvider then
+      vim.bo[bufnr].tagfunc = "v:lua.vim.lsp.tagfunc"
+    end
+
     keymappings(client, bufnr)
   end,
 })
