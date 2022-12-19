5adcba14ca3cf9fa42ecbbe3207fd4ed642b6b89
Richard Roncancio <batusai513@msn.com>
2022-12-19 14:11:22 -0500

    chore: update sumneko_lua config

--- neovim/.config/nvim/lua/plugins/configs/lsp/server_settings/sumneko_lua.lua
+++ neovim/.config/nvim/lua/plugins/configs/lsp/server_settings/sumneko_lua.lua
@@ -1,20 +1,18 @@
-local opts = {
-  settings = {
-    Lua = {
-      workspace = {
-        library = {
-          [vim.fn.stdpath "config" .. "/lua"] = true,
-        },
-      },
-    },
-  },
-}
-
 return function(server, default_opts)
   local neodev_loaded, neodev = pcall(require, "neodev")
   if neodev_loaded then
     neodev.setup {}
   end
+
+  local opts = {
+    settings = {
+      Lua = {
+        completion = {
+          callSnippet = "Replace",
+        },
+      },
+    },
+  }
 
   server.setup(vim.tbl_deep_extend("force", {}, opts, default_opts))
 end
