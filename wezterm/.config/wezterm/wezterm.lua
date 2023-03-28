a5110c33b98fc85c1eaae086ae51925224d4a56f
Richard Roncancio <batusai513@msn.com>
2023-03-28 17:47:14 +0200

    feat: add wezTerm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require "wezterm"
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font = wezterm.font_with_fallback {
+  { family = "JetBrainsMono Nerd Font" },
+  { family = "Symbols Nerd Font" },
+}
+config.use_cap_height_to_scale_fallback_fonts = true
+config.font_size = 16
+config.color_scheme = "tokyonight_moon"
+config.hide_tab_bar_if_only_one_tab = true
+-- config.window_decorations = "RESIZE"
+
+-- and finally, return the configuration to wezterm
+return config
