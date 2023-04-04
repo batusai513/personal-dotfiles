d3a2c3f5ff86930bd38d585107cd24bf90a20758
Richard Roncancio <batusai513@msn.com>
2023-04-04 09:14:13 +0200

    chore: disable lualine for neo-tree

--- neovim/.config/nvim/lua/plugins/configs/lualine/init.lua
+++ neovim/.config/nvim/lua/plugins/configs/lualine/init.lua
@@ -14,7 +14,7 @@ function M.setup()
   lualine.setup {
     options = {
       theme = "auto",
-      disabled_filetypes = { "dashboard", "NvimTree", "Outline", "toggleterm", "lazygit" },
+      disabled_filetypes = { "dashboard", "neo-tree", "Outline", "toggleterm", "lazygit" },
       globalstatus = true,
     },
     extensions = {
