d1c20fb44fe5a1913e0cc17c8dd77ab26c9162ec
Richard Roncancio <batusai513@msn.com>
2023-10-07 08:56:15 +0200

    feat: fixes

--- AstroNvim/.config/astronvim/lua/user/plugins/community.lua
+++ AstroNvim/.config/astronvim/lua/user/plugins/community.lua
@@ -4,9 +4,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.tokyonight" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.pack.typescript" },
