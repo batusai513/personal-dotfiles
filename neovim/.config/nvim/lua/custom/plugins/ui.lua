330f5ec135e4bc21017d1dd56b522ddae757121a
Richard Roncancio <batusai513@msn.com>
2024-03-14 14:30:09 +0100

    feat: add dashboard

--- neovim/.config/nvim/lua/custom/plugins/ui.lua
+++ neovim/.config/nvim/lua/custom/plugins/ui.lua
@@ -59,4 +59,43 @@ return {
     },
     main = 'ibl',
   },
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    enabled = false,
+    opts = function()
+      local logo = [[
+██╗░░░██╗███╗░░░███╗  ░█████╗░░█████╗░██████╗░███████╗
+██║░░░██║████╗░████║  ██╔══██╗██╔══██╗██╔══██╗██╔════╝
+╚██╗░██╔╝██╔████╔██║  ██║░░╚═╝██║░░██║██║░░██║█████╗░░
+░╚████╔╝░██║╚██╔╝██║  ██║░░██╗██║░░██║██║░░██║██╔══╝░░
+░░╚██╔╝░░██║░╚═╝░██║  ╚█████╔╝╚█████╔╝██████╔╝███████╗
+░░░╚═╝░░░╚═╝░░░░░╚═╝  ░╚════╝░░╚════╝░╚═════╝░╚══════╝
+      ]]
+
+      logo = string.rep('\n', 8) .. logo .. '\n\n'
+      local opts = {
+        theme = 'doom',
+        config = {
+          header = vim.split(logo, '\n'),
+          footer = function()
+            local stats = require('lazy').stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
+          end,
+        },
+      }
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == 'lazy' then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'DashboardLoaded',
+          callback = function()
+            require('lazy').show()
+          end,
+        })
+      end
+      return opts
+    end,
+  },
 }
