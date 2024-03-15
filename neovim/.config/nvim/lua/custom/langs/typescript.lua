050687efac7483d9743d049cfc7f06ef5177866a
Richard Roncancio <batusai513@msn.com>
2024-03-15 10:07:37 +0100

    feat: add ts code actions

--- neovim/.config/nvim/lua/custom/langs/typescript.lua
+++ neovim/.config/nvim/lua/custom/langs/typescript.lua
@@ -20,6 +20,34 @@ return {
       'javascriptreact',
     },
     opts = {},
+    keys = {
+      {
+        '<leader>co',
+        function()
+          vim.lsp.buf.code_action {
+            apply = true,
+            context = {
+              only = { 'source.organizeImports.ts' },
+              diagnostics = {},
+            },
+          }
+        end,
+        desc = 'Organize Imports',
+      },
+      {
+        '<leader>cR',
+        function()
+          vim.lsp.buf.code_action {
+            apply = true,
+            context = {
+              only = { 'source.removeUnused.ts' },
+              diagnostics = {},
+            },
+          }
+        end,
+        desc = 'Remove Unused Imports',
+      },
+    },
   },
   {
     'neovim/nvim-lspconfig',
