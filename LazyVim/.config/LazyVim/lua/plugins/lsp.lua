9b2bca0f3641ccf742c62f50c098985e92c9cab6
Richard Roncancio <batusai513@msn.com>
2024-03-16 11:08:29 +0100

    feat: add inlay-hints support to lazyvim

--- LazyVim/.config/LazyVim/lua/plugins/lsp.lua
+++ LazyVim/.config/LazyVim/lua/plugins/lsp.lua
@@ -2,7 +2,30 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      opts.diagnostics.virtual_text = false
+      opts.servers.tsserver.settings.javascript = {
+        inlayHints = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = false,
+        },
+      }
+
+      opts.servers.tsserver.settings.typescript = {
+        inlayHints = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = false,
+        },
+      }
+      -- opts.diagnostics.virtual_text = false
     end,
   },
 }
