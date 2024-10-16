08fb6754ce5643bd1eba830164ad680d468ea94e
Richard Roncancio <batusai513@msn.com>
2024-10-16 23:34:37 +0200

    feat: update wezterm configs

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require "wezterm"
+local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
 local config = {}
@@ -10,20 +10,20 @@ if wezterm.config_builder then
 end
 
 config.font = wezterm.font_with_fallback {
-  { family = "JetBrainsMono Nerd Font" },
-  { family = "Symbols Nerd Font" },
+  { family = 'JetBrainsMono Nerd Font' },
+  { family = 'Symbols Nerd Font' },
 }
 config.use_cap_height_to_scale_fallback_fonts = true
 config.font_size = 16
-config.color_scheme = "tokyonight_moon"
+config.color_scheme = 'Catppuccin Macchiato'
 config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = 'RESIZE'
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+  left = '0.3cell',
+  right = '0.3cell',
+  top = '0.3cell',
+  bottom = '0.3cell',
 }
--- config.window_decorations = "RESIZE"
 
 -- and finally, return the configuration to wezterm
 return config
