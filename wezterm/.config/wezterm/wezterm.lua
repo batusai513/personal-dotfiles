4067b775c6c1878ea2806fb79113de5ec73f86dd
Richard Roncancio <batusai513@msn.com>
2024-10-20 21:58:38 +0200

    feat: add wezterm alt+arrow

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,13 +1,7 @@
 local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback {
   { family = 'JetBrainsMono Nerd Font' },
@@ -23,6 +17,12 @@ config.window_padding = {
   right = '0.3cell',
   top = '0.3cell',
   bottom = '0.3cell',
+}
+config.keys = {
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  { key = 'LeftArrow', mods = 'OPT', action = wezterm.action { SendString = '\x1bb' } },
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  { key = 'RightArrow', mods = 'OPT', action = wezterm.action { SendString = '\x1bf' } },
 }
 
 -- and finally, return the configuration to wezterm
