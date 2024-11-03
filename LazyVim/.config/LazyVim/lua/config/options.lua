cf3a5463634a6c52af8e26561c7302387924fbd5
Richard Roncancio <batusai513@msn.com>
2024-11-03 11:33:09 +0100

    feat: update lazygit config in lazyvim

--- LazyVim/.config/LazyVim/lua/config/options.lua
+++ LazyVim/.config/LazyVim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+vim.g.lazygit_config = false
 
 opt.cmdheight = 0
 opt.foldcolumn = "1"
