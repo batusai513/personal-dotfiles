0bc334c15f9e635040aa4dc0ec666c8a29cd409d
Richard Roncancio <batusai513@msn.com>
2025-06-10 15:41:36 +0200

    feat: update astrovim

--- AstroNvim/.config/astronvim/lua/community.lua
+++ AstroNvim/.config/astronvim/lua/community.lua
@@ -1,3 +1,5 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -6,6 +8,5 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }
