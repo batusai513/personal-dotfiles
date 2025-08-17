f8e9a8ae6e1b8492a89b385e19569095c5aa719c
Richard Roncancio <batusai513@msn.com>
2025-08-17 21:14:11 +0200

    chore: update wezterm settings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -23,13 +23,12 @@ config.font = wezterm.font_with_fallback {
   { family = 'Symbols Nerd Font' },
 }
 -- rendering
-config.front_end = 'WebGpu'
-config.max_fps = 120
--- TODO: change this when unplugged?
-config.webgpu_power_preference = 'HighPerformance'
+-- config.front_end = 'WebGpu'
+-- config.max_fps = 120
+-- config.webgpu_power_preference = 'HighPerformance'
 
 config.use_cap_height_to_scale_fallback_fonts = true
-config.font_size = 15
+config.font_size = 14
 config.color_scheme = 'Catppuccin Macchiato'
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'RESIZE'
